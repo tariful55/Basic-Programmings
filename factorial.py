5abe33f1f357af106822de599ff7791af192ef06
programlife4 <tariful2121@gmail.com>
2023-10-07 18:35:21 +0600

    Find Factorial of a number

---
+++ factorial.py
@@ -0,0 +1,10 @@
+print("Find the fictorial of a given number: ")
+print("Input the  number")
+n=int(input())
+f=1
+x=n
+while x:
+	f=f*x
+	x=x-1
+print("Fictorial of",n, "=", f)
+print("Thank You!")
