6fcd0ae3d340c1188e996ea23d8a94c4d85fa2c4
programlife4 <tariful2121@gmail.com>
2023-10-19 21:43:44 +0600

    Grade point determination based on number gotten in a subject

---
+++ determinate_GradePoint.py
@@ -0,0 +1,14 @@
+x=int(input("Type your mark : "))
+if x>79:
+ print("You have got GPA-5")
+
+elif x>69:
+ print("You have got GPA-4")
+
+elif x>59:
+ print("You have got GPA-3.5")
+
+elif x>49:
+ print("You have got GPA-3")
+else : 
+ print("You have failed")
