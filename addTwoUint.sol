94407b19759d55b0b552ad7a486c5657de54c8ab
programlife4 <tariful2121@gmail.com>
2023-11-15 01:24:12 +0600

    First Solidity Prgamm: Hello world printing

---
+++ addTwoUint.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0 ;
+contract sum{
+    function addTwoNumbers() public pure returns (uint256){
+        uint256 x = 10;
+        uint256 y = 20;
+        return x + y;
+    }
+}
